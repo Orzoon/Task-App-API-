cbaa1fb9c1b96768b4ef232f6c215f13fa2c71e4
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-09 20:24:43 +1100

    dsd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
-
+const cors = require('cors');
 
 
 
@@ -11,12 +11,7 @@ const port = process.env.PORT;
 
 
 app.use(express.json());
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','*');
-    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-})
+app.use(cors())
 app.use(userRouter);
 app.use(taskRouter);
 
