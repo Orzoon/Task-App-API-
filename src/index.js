efcd74d3c41218e8bb2848174586b3f3a6d56aa4
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-09 20:13:47 +1100

    finalchange

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','*');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 })
 app.use(userRouter);
