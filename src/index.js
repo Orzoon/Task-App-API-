fc9694360889b69d60a382511b00318c075e34be
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-09 19:49:49 +1100

    changes again

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use((req,res,next) => {
     res.setHeader('Acess-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, UPDATE,DELETE')
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, UPDATE,DELETE');\
+    next();
 })
 app.use(userRouter);
 app.use(taskRouter);
