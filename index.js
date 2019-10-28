567697836f88e3c8f105be91d5fd8c03f00663e7
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-28 19:19:22 +1100

    .....

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const cors = require('cors');
 const path = require('path')
 
 
-//from server
+
 const app = express();
 const port = process.env.PORT;
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/*', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
+app.get('/*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log("server is up and running")
