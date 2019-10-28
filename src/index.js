aedbd6503ae42e3443012d6e23b4e90e96397f47
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-28 15:33:23 +1100

    ....changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const cors = require('cors');
+const path = require('path')
 
 
 
 const app = express();
 const port = process.env.PORT;
 
-
+app.use('/', express.static('public/index.html'));
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
---
+++ src/public/precache-manifest.c725961a60b460fae986b1a3549c7df2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2892ea3094d43d1a3dbe2752392f626f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bfb7cd9d95ee09ee8a",
+    "url": "/static/css/main.568554d7.chunk.css"
+  },
+  {
+    "revision": "8679825c3dc254d2271f",
+    "url": "/static/js/2.4f1445cf.chunk.js"
+  },
+  {
+    "revision": "08bfb7cd9d95ee09ee8a",
+    "url": "/static/js/main.074745f6.chunk.js"
+  },
+  {
+    "revision": "a79c9c79b7a534ee7d36",
+    "url": "/static/js/runtime-main.ee5d8376.js"
+  }
+]);
