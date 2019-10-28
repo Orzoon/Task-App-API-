aa7b3dc556bedcfad17a744b41d9226acf2bbe98
orzoon <orzoonkunwar7.ak@gmail.com>
2019-10-28 16:35:23 +1100

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+require('./src/db/mongoose');
+const userRouter = require('./src/routers/user');
+const taskRouter = require('./src/routers/task');
 const cors = require('cors');
 const path = require('path')
 
@@ -9,15 +9,15 @@ const path = require('path')
 
 const app = express();
 const port = process.env.PORT;
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.get('/', (req,res) => {
-    res.sendfile(path.join(__dirname +'../public/index.html'));
-})
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 app.listen(port, () => {
     console.log("server is up and running")
 
